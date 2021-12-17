322b781b7dc23558ceb07f48ca4e8ef48f7256c4
gonend <48886619+gonend@users.noreply.github.com>
2021-12-17 17:32:41 +0200

    friday, gonen: added new pics+fixed front design weather api function

--- mybackend.py
+++ mybackend.py
@@ -39,12 +39,6 @@ def check_location_in_db(location_name):
         ret_val = cur.fetchall()
         return len(ret_val) > 0
 
-def get_lat_log_for_location(location_names):
-    con = lite.connect('recommendation.db')
-    with con:
-        cur = con.cursor()
-        cur.execute('FROM ')
-        return len(ret_val) > 0
 
 def get_location_for_recommendation(start_location, time_of_trip, num_of_recommendation):
     """
